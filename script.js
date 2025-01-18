3547e747bc154502aebe5587dba8e23fde7d8c50
Rajan Himang <rajanhimang@gmail.com>
2025-01-19 10:06:14 +1100

    Cleaned code

---
+++ script.js
@@ -0,0 +1,48 @@
+import { API_KEY } from "./config.js";
+
+const recipeListEl = document.getElementById("recipe-list");
+
+const displayRecipes = function (recipes) {
+  recipeListEl.innerHTML = "";
+  recipes.forEach((recipe) => {
+    const recipeItemEl = document.createElement("li");
+    recipeItemEl.classList.add("recipe-item");
+
+    const recipeImageEl = document.createElement("img");
+    recipeImageEl.src = recipe.image;
+    recipeImageEl.alt = "recipe image";
+
+    const recipeTitleEl = document.createElement("h2");
+    recipeTitleEl.innerText = recipe.title;
+
+    const recipeIngredientEl = document.createElement("p");
+    recipeIngredientEl.innerHTML = `<strong>Ingredients:</Strong> ${recipe.extendedIngredients
+      .map((ingredient) => ingredient.original)
+      .join(", ")}`;
+
+    const recipeLinkEl = document.createElement("a");
+    recipeLinkEl.href = recipe.sourceUrl;
+    recipeLinkEl.innerText = "View Recipe";
+
+    recipeItemEl.appendChild(recipeImageEl);
+    recipeItemEl.appendChild(recipeTitleEl);
+    recipeItemEl.appendChild(recipeIngredientEl);
+    recipeItemEl.appendChild(recipeLinkEl);
+    recipeListEl.appendChild(recipeItemEl);
+  });
+};
+
+const getRecipes = async function () {
+  const response = await fetch(
+    `https://api.spoonacular.com/recipes/random?number=10&apiKey=${API_KEY}`
+  );
+  const data = await response.json();
+  return data.recipes;
+};
+
+const init = async function () {
+  const recipes = await getRecipes();
+  displayRecipes(recipes);
+};
+
+init();
